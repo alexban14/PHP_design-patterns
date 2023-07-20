b9cf711a7e55958040dbf9bf9de7970cb02a8bf1
BanAlexandru <alexandru.ban@gsdgroup.net>
2023-07-20 12:46:53 +0300

    abstract factory pattern with real world example

--- AbstractFactory.php
+++ AbstractFactory.php
@@ -21,7 +21,7 @@ interface TemplateFactory
 
 class TwigTemplateFactory implements TemplateFactory
 {
-	public function function createTitleTemplate(): TitleTemplate
+	public function createTitleTemplate(): TitleTemplate
 	{
 		return new TwigTitleTemplate();
 	}
@@ -113,3 +113,125 @@ abstract class BasePageTemplate implements PageTemplate
 		$this->titleTemplate = $titleTemplate;
 	}
 }
+
+/**
+ * The Twig variant of the whole page templates.
+ */
+class TwigPageTemplate extends BasePageTemplate
+{
+	public function getTemplateString(): string
+	{
+		$renderedTitle = $this->titleTemplate->getTemplateString();
+
+		return <<<HTML
+		<div class="page">
+			$renderedTitle
+			<article class="content">{{ content }} </article>
+		</div>
+		HTML;
+	}
+}
+
+
+/**
+ * The PHPTemplate variant of the whole page templates.
+ */
+class PHPTemplatePageTemplate extends BasePageTemplate
+{
+	public function getTemplateString(): string
+	{
+		$renderedTitle = $this->titleTemplate->getTemplateString();
+
+		return <<<HTML
+		<div class="page">
+			$renderedTitle
+			<article class="content"><? \$content ?></article>
+		</div>
+		HTML;
+	}
+}
+
+/**
+ * The renderer is responsible for converting a template string into the actual
+ * HTML code. Each renderer behaves differently and expects its own type of
+ * template strings passed to it. Baking templates with the factory let you pass
+ * proper types of templates to proper renders.
+ */
+interface TemplateRenderer
+{
+	public function render(string $templateString, array $arguments = []): string;
+}
+
+/**
+ * Renderer for Twig templates
+ */
+class TwigRenderer implements TemplateRenderer
+{
+	public function render(string $templateString, array $arguments = []): string
+	{
+		return \Twig::render($templateString, $arguments);
+	}
+}
+
+/**
+ * Renderer for PHPTemplate templates
+ */
+class PHPTemplateRenderer implements TemplateRenderer
+{
+	public function render(string $templateString, array $arguments = []): string
+	{
+		extract($arguments);
+
+		ob_start();
+		eval(' ?>' . $templateString . '<?php ');
+		$result = ob_get_contents();
+		ob_end_clean();
+
+		return $result;
+	}
+}
+
+/**
+ * The client code. Note that it accepts the Abstract Factory class as the
+ * parameter, which allows the client to work with any concrete factory type.
+ */
+class Page
+{
+	public $title;
+
+	public $content;
+
+	public function __construct($title, $content)
+	{
+		$this->title = $title;
+		$this->content = $content;
+	}
+
+    // Here's how would you use the template further in real life. Note that the
+    // page class does not depend on any concrete template classes.
+	public function render(TemplateFactory $factory): string
+	{
+		$pageTemplate = $factory->createPageTemplate();
+
+		$renderer = $factory->getRenderer();
+
+		return $renderer->render($pageTemplate->getTemplateString(), [
+			'title' => $this->title,
+			'content' => $this->content
+		]);
+	}
+}
+
+/**
+ * Now in other parts of the app, the client code can accept factory objects of any type
+ */
+$page = new Page('Sample Page', 'This is the body');
+
+echo "Testing actula rendering with PHPTemplate factory: \n";
+echo $page->render(new PHPTemplateFactory());
+
+
+// Uncomment the following if you have Twig installed.
+
+// echo "Testing rendering with the Twig factory:\n"; echo $page->render(new
+// TwigTemplateFactory());
